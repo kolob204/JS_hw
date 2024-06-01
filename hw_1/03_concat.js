67f4c9410be42c3dd0ee6f7397135570eb9ea910
viktor <elastic@yandex.ru>
2024-06-01 22:04:46 +0400

    first try of HomeWork2 (JS)

--- hw_1/03_concat.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const concatVariables = (n) => {
-  //return n+ +`${String(n).repeat(2)}` + +`${String(n).repeat(3)}`;
-  return +(n)+ +`${n.repeat(2)}` + +`${n.repeat(3)}`;
-}
-
-//console.log(concatVariables(5));
-console.log(concatVariables("5"));
---
+++ hw_2/task_1_if.js
@@ -0,0 +1,15 @@
+const age = 1;
+
+const age2 = 18;
+
+const age3 = 60;
+
+if(age<age2 && age>1) {
+    console.log("You don't have access cause your age is " + age + " i'ts less then " )
+} else if(age>=age2 && age < age3) {
+    console.log("Welcome")
+} else if(age>age3) {
+    console.log("Keep calm and look culture channel" )
+} else {
+    console.log("Technical works" )
+}
